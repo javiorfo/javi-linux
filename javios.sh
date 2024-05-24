80386b9dfbb0bb18278ce595ac7c9734550adcfe
Javier Orfo <124217829+javiorfo@users.noreply.github.com>
2024-05-23 23:22:46 -0300

    Update javios.sh

--- javios.sh
+++ javios.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 # BASE
-sudo pacman --needed -S xorg, base-devel, curl, wget, git, nsxiv, unzip, xorg-xrandr, dosfstools, dmenu, xclip, xautolock, xorg-xinit, alacritty, vi, less, scrot, startx, cmake, figlet, tree, imagemagick
+sudo pacman --needed -S xorg, base-devel, curl, wget, git, nsxiv, unzip, xorg-xrandr, dosfstools, dmenu, xclip, xautolock, xorg-xinit, vi, less, scrot, startx, figlet, tree, imagemagick
 
 # OFFICE
 sudo pacman --needed -S libreoffice-still, ttf-iosevka-nerd, zathura, zathura-pdf-mupdf, gimp, translate-shell
@@ -10,13 +10,13 @@ sudo pacman --needed -S libreoffice-still, ttf-iosevka-nerd, zathura, zathura-pd
 sudo pacman --needed -S bluez, bluez-utils, mpv, transmission-cli, neomutt
 
 # PROGRAMMING
-lua, lua-language-server, jdk21-openjdk, rust, rust-analyzer, clang, netcat, tree, lsof, maven, gradle, neovim, jq, tidy, docker, docker-compose, docker-buildx, minikube, kubectl, ripgrep, jwt-cli, plantuml
+lua, lua-language-server, jdk21-openjdk, go, gopls, clang, netcat, tree, lsof, maven, gradle, neovim, jq, tidy, docker, docker-compose, docker-buildx, minikube, kubectl, ripgrep, jwt-cli, plantuml
 
 # OPTIONAL
 # sudo pacman --needed -S brightnessctl
 
 # AUR
-paru -S hydroxide, jdtls, presenterm-bin, jdk21-graalvm-ee-bin, lombok-common, java-debug
+yay -S hydroxide, jdtls, slides, jdk21-graalvm-ee-bin, lombok-common, java-debug
 
 # Externos
-paru, dwm
+yay, dwm, st
