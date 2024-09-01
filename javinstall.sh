04546eff1e8896e041d3263ef7b48f6324c6e8c9
Javier Orfo <javierorfo@prontonmail.com>
2024-09-01 11:03:39 -0300

    cleaning

--- javinstall.sh
+++ javinstall.sh
@@ -8,24 +8,50 @@ echo -e " _/ |\__,_| \_/ |_|     |_|_|_| |_|\__,_/_/\_\ "
 echo -e "|__/                                           "
 echo ""
 
+##### FUNCTIONS #####
+ask_yes_no() {
+    while true; do
+        read -p "$1 (y/n): " answer
+        case $answer in
+            [Yy]* ) return 0;;
+            [Nn]* ) return 1;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+}
+##### END #####
+
+
+##### LAPTOP #####
+if ask_yes_no "Is it a laptop?"; then
+    echo "Installing laptop software..."
+    sudo pacman --needed --noconfirm -S brightnessctl
+    echo "Done!"
+fi
+##### END #####
+
+
+##### PACMAN #####
 echo "Installing base..."
-sudo pacman --needed -S xorg base-devel curl wget git nsxiv unzip xorg-xrandr dosfstools dmenu xclip \
+sudo pacman --needed --noconfirm -S xorg base-devel curl wget git nsxiv unzip xorg-xrandr dosfstools dmenu xclip firefox \
     xautolock xorg-xinit vi less scrot startx figlet tree imagemagick alacritty pass lf peek bluez bluez-utils mpv transmission-cli
 echo "Done!"
 
 echo "Installing office apps..."
-sudo pacman --needed -S libreoffice-still ttf-iosevka-nerd zathura zathura-pdf-mupdf gimp translate-shell texlive-latexextra texlive-binextra
+sudo pacman --needed --noconfirm -S libreoffice-still ttf-iosevka-nerd zathura zathura-pdf-mupdf gimp translate-shell texlive-latexextra texlive-binextra
 echo "Done!"
-
-# If is notebook
-sudo pacman --needed -S brightnessctl
 
 echo "Installing programming tools..."
-sudo pacman --needed -S lua lua-language-server jdk21-openjdk go gopls rust rust-analyzer \
-    delve clang netcat lsof maven neovim jq tidy docker docker-compose docker-buildx minikube kubectl ripgrep jwt-cli plantuml gdb
+sudo pacman --needed --noconfirm -S neovim lua lua-language-server jdk21-openjdk go gopls rust rust-analyzer \
+    delve clang netcat lsof maven jq tidy docker docker-compose docker-buildx minikube kubectl ripgrep jwt-cli plantuml gdb
 echo "Done!"
+##### END #####
 
-# Install external
+
+cd ~
+
+
+##### EXTERNAL #####
 echo "Installing dwm..."
 git clone https://github.com/javiorfo/dwm
 cd dwm
@@ -40,10 +66,14 @@ sudo make clean install
 cd ..
 echo "Done!"
 
-echo "Installing nvim config..."
+echo "Setting nvim config..."
 cd .config
 git clone https://github.com/javiorfo/nvim
 cd ..
+echo "Done!"
+
+echo "Setting bash config..."
+wget https://github.com/javiorfo/dotfiles/blob/master/.bashrc
 echo "Done!"
 
 echo "Installing yay..."
@@ -53,11 +83,27 @@ makepkg -si
 cd ..
 rm -rdf yay
 echo "Done!"
+##### END #####
 
+
+##### AUR #####
 echo "Installing programs from AUR..."
-yay -S jdtls slides jdk21-graalvm-ee-bin lombok-common java-debug codelldb-bin
+yay --noconfirm -S jdtls slides jdk21-graalvm-ee-bin lombok-common java-debug codelldb-bin
+echo "Done!"
+##### END #####
+
+
+##### CONFIG FILES #####
+echo "Installing config files..."
+sudo cp javi-linux/files/.bash_profile .
+sudo cp javi-linux/files/.xinitrc .
+sudo cp javi-linux/files/00-keyboard.conf /etc/X11/xorg.conf.d/
 echo "Done!"
 
-# Configure files folder
-# Bluetooth config
-# Bash
+echo "Setting bluetooth config..."
+sudo sed -i 's/#AutoEnable=true/AutoEnable=true/g' /etc/bluetooth/main.conf
+echo "Done!"
+##### END #####
+
+
+echo "javi-linux finished. Reboot your computer."
