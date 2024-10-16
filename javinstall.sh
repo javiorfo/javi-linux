66811ad16c2bcef4133b8095ed1e470bfcc9e32b
Javier Orfo <124217829+javiorfo@users.noreply.github.com>
2024-10-16 12:39:49 -0300

    Update javinstall.sh

--- javinstall.sh
+++ javinstall.sh
@@ -45,7 +45,7 @@ echo "Done!"
 echo "Installing programming tools..."
 sudo pacman --needed --noconfirm -S neovim lua lua-language-server jdk21-openjdk go gopls \
     rust rust-analyzer delve clang netcat lsof maven jq tidy docker docker-compose docker-buildx \
-    minikube kubectl ripgrep jwt-cli plantuml gdb unixodbc
+    zig, zls, minikube kubectl ripgrep jwt-cli plantuml gdb unixodbc
 echo "Done!"
 ##### END #####
 
