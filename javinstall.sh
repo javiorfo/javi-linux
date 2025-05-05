ef7a26234ccaa6702bc88e02074349a6e6acb000
Javier Orfo <javierorfo@protonmail.com>
2025-05-05 15:16:50 -0300

    Update javinstall.sh

--- javinstall.sh
+++ javinstall.sh
@@ -34,7 +34,7 @@ fi
 ##### PACMAN #####
 echo "Installing base..."
 sudo pacman --needed --noconfirm -S xorg base-devel curl wget nsxiv feh unzip xorg-xrandr \
-    dosfstools dmenu xclip xautolock xorg-xinit xorg-xsetroot vi less scrot figlet \
+    dosfstools dmenu xclip xautolock xorg-xinit xorg-xsetroot vi less scrot figlet ueberzugpp \
     tree imagemagick alacritty peek bluez bluez-utils mpv transmission-cli ripgrep
 echo "Done!"
 
